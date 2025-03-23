f7630f50d0f7bbc7f9908d61d4109ce128d08d63
ionut <ionut.roma9@gmail.com>
2025-03-23 17:48:22 +0100

    footer

--- hackathon25/src/views/Home.vue
+++ hackathon25/src/views/Home.vue
@@ -3,6 +3,7 @@ import HeroArea from '@/components/HeroArea.vue';
 import AboutComponent from '@/components/AboutComponent.vue';
 import RequimentsComponent from '@/components/RequimentsComponent.vue';
 import CarouselView from './CarouselView.vue';
+import Footer from '@/components/Footer.vue';
 </script>
 
 <template>
